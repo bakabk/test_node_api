386448534a847a1f8a980b1e31e0499babebb6cf
Yan.C Work <baka_@bk.ru>
2018-01-16 16:15:59 +0300

    edit MC

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var ObjectID = require('mongodb').ObjectID;
+var artistsController = require('./controllers/artists')
 
 var db = require('./db');
 
@@ -10,92 +10,31 @@ var url = 'mongodb://localhost:27017/';
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//test GET api
 app.get('/', function(req, res){
   res.send('Hellow API!')
 });
 
-var artists = [
-  {
-    id: 1,
-    name: 'Metallica'
-  },
-  {
-    id: 2,
-    name: 'Iron Maden'
-  },
-  {
-    id: 3,
-    name: 'Deep Purple'
-  }
-];
+//getAll GET artist
+app.get('/artists', artistsController.all);
 
-app.get('/artists', function(req, res){
-  db.get().collection('artists').find().toArray(function(err, docs){
-    if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    res.send(docs);
-  })
-});
-
-//get GET artist
-app.get('/artists/:id', function (req, res) {
-  db.get().collection('artists').findOne({ _id: ObjectID(req.params.id) }, function (err, doc) {
-    if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    res.send(doc);
-  })
-})
+//find GET artist
+app.get('/artists/:id', artistsController.findById)
 
 //add POST artists
-app.post('/artists', function (req, res) {
-  var artist = {
-    name: req.body.name
-  };
+app.post('/artists', artistsController.create)
 
-  db.get().collection('artists').insert(artist, (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.sendStatus(500);
-    }
-    res.send(artist);
-  })
-})
+//updatre PUT artist
+app.put('/artists/:id', artistsController.update)
 
-app.put('/artists/:id', function (req, res) {
-  db.get().collection('artists').update(
-    { _id: ObjectID(req.params.id) },
-    { name: req.body.name },
-    function (err, result) {
-      if (err) {
-        console.log(err);
-        return res.sendStatus(500);
-      }
-      res.sendStatus(200);
-    })
-})
-
-app.delete('/artists/:id', function(req, res){
-  db.get().collection('artists').deleteOne(
-    { _id: ObjectID(req.params.id) },
-    function (err, result) {
-      if (err) {
-        console.log(err);
-        return res.sendStatus(500);
-      }
-      res.sendStatus(200);
-    }
-  )
-})
+//delete DELETE artis
+app.delete('/artists/:id', artistsController.delete)
 
 db.connect(url, function (err, database) {
   if (err) {
     return console.log(err);
   }
-  // db = database.db("mydb");
+
   app.listen(3012, function () {
     console.log('API app started');
     console.log('Mongo started');
